edc5eb65676b7fdb6fdd8608d67f2aa5a1cea58f
Prakash Sonawane <830157@cognizant.com>
2020-01-02 17:13:53 +0530

    my first project

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 class Training{  
     public static void main(String args[]){  
-     System.out.println("Hello World...Good morning");  
+     System.out.println("New project started");  
     }  
 }  
